15a84317bfb5e6f05c1d35cb681a7e760375c842
Nikh1ll-04 <nk4377001@gmail.com>
2026-07-13 23:02:16 +0530

    Initial Leetcode push

--- Concat_of_array.cpp
+++ Concat_of_array.cpp
@@ -1,32 +1,32 @@
-// #include<iostream>
-// #include<vector>
-// using namespace std;
-// vector<int> Concat(vector<int> vec , int n , vector<int> ans){
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> Concat(vector<int> vec , int n , vector<int> ans){
     
-//     for(int  i = 0 ; i < 2 * n ; i++){
-//         if(i < n){
-//             ans.push_back(vec[i]);
-//         }else{
-//             ans.push_back(vec[i - n]);
-//         }
-//     }
-//     return ans;
-// }
-// int main(){
-//     int n;
-//     cout<<"Enter the number of element : ";
-//     cin>>n;
-//     vector<int > vec;
-//     for(int  i = 0; i < n ; i++){
-//         int x;
-//         cin>>x;
-//         vec.push_back(x);
-//     }
-//     vector<int > ans;
-//     cout<<"[";
-//     vector<int> arr = Concat(vec , n, ans);
-//     for(auto v : arr ){
-//         cout<< v <<" ";
-//     }
-//     cout<<"]";
-// }
+    for(int  i = 0 ; i < 2 * n ; i++){
+        if(i < n){
+            ans.push_back(vec[i]);
+        }else{
+            ans.push_back(vec[i - n]);
+        }
+    }
+    return ans;
+}
+int main(){
+    int n;
+    cout<<"Enter the number of element : ";
+    cin>>n;
+    vector<int > vec;
+    for(int  i = 0; i < n ; i++){
+        int x;
+        cin>>x;
+        vec.push_back(x);
+    }
+    vector<int > ans;
+    cout<<"[";
+    vector<int> arr = Concat(vec , n, ans);
+    for(auto v : arr ){
+        cout<< v <<" ";
+    }
+    cout<<"]";
+}
